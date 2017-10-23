ae8a2ac30644604402344993328d534ff3d9a05d
Raz Friman <raz.friman@razfriman.com>
2017-10-23 12:48:59 -0500

    Save missing file

--- RazzleServer/Handlers/CharacterListHandler.cs
+++ RazzleServer/Handlers/CharacterListHandler.cs
@@ -25,10 +25,9 @@ namespace RazzleServer.Handlers
             var chars = acc.GetCharsFromDatabase();
 
             var pw = new PacketWriter(); pw.WriteHeader(SMSGHeader.CHARLIST);
-
             pw.WriteByte(0);
             pw.WriteByte((byte)chars.Count);
-            chars.ForEach(chr => MapleCharacter.AddCharEntry(pw, chr);
+            chars.ForEach(chr => MapleCharacter.AddCharEntry(pw, chr));
             pw.WriteByte(1);
             pw.WriteInt(acc.CharacterSlots);
             return pw;
