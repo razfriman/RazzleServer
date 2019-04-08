da68d7ee6f8e980db5cf932e8bc03e0da9fa659e
Raz Friman <raz.friman@razfriman.com>
2019-04-08 20:18:37 +1000

    Add world status test

--- RazzleServer.Tests/LoginClientIntegrationTest.cs
+++ RazzleServer.Tests/LoginClientIntegrationTest.cs
@@ -68,6 +68,58 @@ namespace RazzleServer.Tests
             SendLogin(client, username, "xxxxxx", LoginResult.InvalidPassword);
         }
 
+        [TestMethod]
+        public void Login_Valid_SendsWorldStatus()
+        {
+            ServerConfig.Instance.EnableAutoRegister = true;
+            var client = _server.AddFakeLoginClient();
+            var username = "admin";
+            var password = "admin";
+            SendLogin(client, username, password, LoginResult.InvalidUsername);
+            SendLogin(client, username, password, LoginResult.Valid);
+            CheckWorldInformation(client, false);    
+            CheckWorldInformation(client, true);
+        }
+
+        private void CheckWorldInformation(FakeLoginClient client, bool isEnd)
+        {
+            var packetEvent = new ManualResetEventSlim();
+
+            void WorldInformationHandler(PacketReader packet)
+            {
+                if (packetEvent.IsSet)
+                {
+                    return;
+                }
+                
+                var header = (ServerOperationCode)packet.ReadByte();
+                Assert.AreEqual(ServerOperationCode.WorldInformation, header);
+                
+                var worldId = packet.ReadByte();
+
+                if (isEnd)
+                {
+                    Assert.AreEqual(0xFF, worldId);
+                }
+                else
+                {
+                    Assert.IsTrue(client.Server.Manager.Worlds.Contains(worldId));
+                    var world = client.Server.Manager.Worlds[worldId];
+                    var worldName = packet.ReadString();
+                    var channels = packet.ReadByte();
+                    
+                    Assert.AreEqual(world.Name, worldName);
+                    Assert.AreEqual(world.Channels, channels);
+                }
+
+                packetEvent.Set();
+                client.ServerToClientPacket -= WorldInformationHandler;
+            }
+
+            client.ServerToClientPacket += WorldInformationHandler;
+            packetEvent.Wait(Timeout);
+        }
+
         private static void SendLogin(FakeLoginClient client, string username, string password,
             LoginResult expectedResult)
         {
