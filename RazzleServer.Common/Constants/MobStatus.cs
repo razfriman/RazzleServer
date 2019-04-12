96c5a735011ae7b4544f235ee7b2ba7d8b1e043e
Raz Friman <raz.friman@razfriman.com>
2019-04-13 09:10:03 +1000

    Update mob status

--- RazzleServer.Common/Constants/MobStatus.cs
+++ RazzleServer.Common/Constants/MobStatus.cs
@@ -11,17 +11,18 @@ namespace RazzleServer.Common.Constants
         WeaponDefenceIcon = 0x02,
         MagicAttackIcon = 0x04,
         MagicDefenceIcon = 0x08,
+        
         AccuracyIcon = 0x10,
         AvoidabilityIcon = 0x20,
         SpeedIcon = 0x40,
-
         Stunned = 0x80,
+        
         Frozen = 0x100,
         Poisoned = 0x200,
         Sealed = 0x400,
-
         Unknown1 = 0x800,
 
+       
         WeaponAttackUp = 0x1000,
         WeaponDefenseUp = 0x2000,
         MagicAttackUp = 0x4000,
@@ -29,18 +30,19 @@ namespace RazzleServer.Common.Constants
 
         Doom = 0x10000,
         ShadowWeb = 0x20000,
-
         WeaponImmunity = 0x40000,
         MagicImmunity = 0x80000,
 
         Unknown2 = 0x100000,
-        Unknown3 = 0x200000,
+        HardSkin = 0x200000,
         NinjaAmbush = 0x400000,
-        Unknown4 = 0x800000,
+        ElementalAttribute = 0x800000, 
+
         VenomousWeapon = 0x1000000,
-        Unknown5 = 0x2000000,
-        Unknown6 = 0x4000000,
+        Blind = 0x2000000,
+        SealSkill = 0x4000000,
         Empty = 0x8000000,
+        
         Hypnotized = 0x10000000,
         WeaponDamageReflect = 0x20000000,
         MagicDamageReflect = 0x40000000
